9f53f6301d16370669a03ddfcf747e7d91a3363d
luizalano <euluizalano@gmail.com>
2020-08-31 21:09:55 -0300

    Commit inicial

---
+++ tipoVinhoFrame.py
@@ -0,0 +1,124 @@
+# coding: utf-8
+from tipovinho import *
+from pacotesMG.conectaDataBaseMG import *
+
+from pacotesMG.wxComponetesMG import FrameMG
+import wx
+import wx.grid as gridlib
+
+
+class FrmTipoVinho(FrameMG):
+    insert = False
+    caminho = '..\\pacotesMG\\icones\\'
+
+    def __init__(self):
+        self.tipoVinho = TipoVinho()
+
+        super(FrmTipoVinho, self).__init__(None, 'Cadastro de tipos de vinhos', 800, 700, 0)
+
+        self.criaComponentes()
+
+    def criaComponentes(self):
+        X = self.posx(1)
+        Y = self.posy(0)
+        tamX = self.larguraEmPx(106)
+        tamY = self.alturaEmPx(12)
+
+        self.grid = wx.ListCtrl(self.painel, pos=(X, Y), size=(tamX, tamY),
+                                style=wx.LC_REPORT | wx.LC_VRULES | wx.LC_HRULES | wx.BORDER_SUNKEN)
+
+        label01, self.txtId = self.criaCaixaDeTexto(self.painel, 12, 10, 40, 'ID', 0, xcol=1, tamanho = 6)
+        label02, self.txtNomeTipoVinho = self.criaCaixaDeTexto(self.painel, 13, 80, 520, 'Nome do tipo de vinho',
+                                                             self.tipoVinho.sqlBuscaTamanho('nometipovinho'), xcol = 1, tamanho = 90)
+
+        self.limpaElementos()
+
+        # self.grid.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.selecionaLinha)
+        self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.selecionaLinha, self.grid)
+
+        self.encheGrid()
+
+        self.Show()
+
+    def encheGrid(self):
+        '''
+        u.idtipoVinho, u.nometipoVinho, u.corcasca, u.idpais, p.nomepais
+        '''
+        self.grid.ClearAll()
+        self.grid.InsertColumn(0, 'id', width=self.larguraEmPx(6))
+        self.grid.InsertColumn(1, 'Nome do tipo de vinho', width=self.larguraEmPx(90))
+
+        self.lista = self.tipoVinho.getAll()
+
+        for row in self.lista:
+            self.grid.Append([row[0], row[1]])
+
+    def limpaElementos(self):
+        self.txtId.Clear()
+        self.txtNomeTipoVinho.Clear()
+
+        self.txtId.Disable()
+        self.txtNomeTipoVinho.Disable()
+
+        self.botaoSalva.Disable()
+        self.botaoDelete.Disable()
+
+    def selecionaLinha(self, event):
+        item = self.grid.GetFocusedItem()
+
+        idTipoVinho = self.grid.GetItemText(item, 0)
+
+        if idTipoVinho.isdigit():
+            lista = self.tipoVinho.buscaTipoVinho(idTipoVinho)
+
+            self.txtId.SetValue(str(lista[0]))
+            self.txtNomeTipoVinho.SetValue(lista[1])
+
+            self.txtNomeTipoVinho.Enable()
+
+            self.botaoSalva.Enable()
+            self.botaoDelete.Enable()
+
+    def salvaElemento(self, event):
+        self.tipoVinho.setnomeTipoVinho(self.txtNomeTipoVinho.GetValue())
+
+        if self.insert:
+            self.tipoVinho.insere()
+            self.insert = False
+        else:
+            self.tipoVinho.update(str(self.txtId.GetValue()))
+
+        self.limpaElementos()
+
+        self.encheGrid()
+
+    def deletaElemento(self, event):
+        super(FrmTipoVinho, self).deletaElemento(event)
+        if self.prossegueEliminacao:
+            self.tipoVinho.delete(str(self.txtId.GetValue()))
+
+            self.limpaElementos()
+
+            self.encheGrid()
+
+    def habilitaNovo(self, event):
+        self.limpaElementos()
+
+        self.txtNomeTipoVinho.Enable()
+
+        self.botaoSalva.Enable()
+
+        self.insert = True
+
+    def cancelaOperacao(self, event):
+        self.limpaElementos()
+
+def main():
+    app = wx.App()
+    frmTipoVinho = FrmTipoVinho()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ tipovinho.py
@@ -0,0 +1,140 @@
+# coding: utf-8
+from pacotesMG.conectaDataBaseMG import *
+from pacotesMG.diversos import *
+
+
+class TipoVinho():
+    idTipoVinho = 0
+    nomeTipoVinho = ''
+    banco = 'mysql'
+
+    def __init__(self):
+        self.con, self.cursor = conectaMySql('masteradega', 'Adeg@W!ne1', 'adega.mysql.uhserver.com', 'adega')
+        if self.con == None:
+            sys.exit()
+
+    def getAll(self):
+        clausulaSql  = 'select idtipovinho, nometipovinho '
+        clausulaSql += 'from tipovinho  '
+        clausulaSql += 'order by nometipovinho;'
+
+        try:
+            self.cursor.execute(clausulaSql)
+        except:
+            dlg = wx.MessageDialog(None, clausulaSql, 'Erro ao ler tipos de vinhos', wx.OK | wx.ICON_ERROR)
+            result = dlg.ShowModal()
+
+        lista = []
+
+        row = self.cursor.fetchone()
+        while row != None:
+            lista.append([row[0], row[1]])
+
+            row = self.cursor.fetchone()
+
+        return lista
+
+    def pesquisaTipoVinho(self, chave):
+        clausulaSql = "select idtipovinho, nometipovinho from tipoVinho where "
+        clausulaSql += " upper(nometipovinho) like upper('%" + chave + "%') order by nometipovinho;"
+
+        try:
+            self.cursor.execute(clausulaSql)
+        except:
+            dlg = wx.MessageDialog(None, clausulaSql, 'Erro ao pesquisar tipo de vinho', wx.OK | wx.ICON_ERROR)
+            result = dlg.ShowModal()
+
+        lista = []
+
+        row = self.cursor.fetchone()
+        while row != None:
+            lista.append(str(row[0]) + '|' + row[1])
+
+            row = self.cursor.fetchone()
+
+        return lista
+
+    def buscaTipoVinho(self, argId):
+        clausulaSql  = 'select idtipovinho, nometipovinho '
+        clausulaSql += 'from tipovinho '
+        clausulaSql += 'where idtipovinho = '
+        clausulaSql += str(argId) + ';'
+
+        try:
+            self.cursor.execute(clausulaSql)
+        except:
+            dlg = wx.MessageDialog(None, clausulaSql, 'Erro ao buscar tipo de vinho com ID ' + str(argId) + '!',
+                                   wx.OK | wx.ICON_ERROR)
+            result = dlg.ShowModal()
+
+        lista = []
+
+        row = self.cursor.fetchone()
+        if row != None:
+            lista.append(row[0])
+            lista.append(row[1])
+            lista.append(row[2])
+            lista.append(row[3])
+            lista.append(row[4])
+
+        return lista
+
+    def setidTipoVinho(self, arg):
+        self.idTipoVinho = arg
+
+    def setnomeTipoVinho(self, arg):
+        self.nomeTipoVinho = arg
+
+    def insere(self):
+        clausulaSql = "insert into tipovinho (nometipovinho) values ("
+        clausulaSql += "'" + tiraAspas(self.nomeTipoVinho) + "');"
+
+        try:
+            self.cursor.execute(clausulaSql)
+            self.con.commit()
+        except:
+            dlg = wx.MessageDialog(None, clausulaSql, 'Erro ao inserir dados do tipo de vinho!', wx.OK | wx.ICON_ERROR)
+            result = dlg.ShowModal()
+
+    def update(self, argId):
+        clausulaSql = "update tipovinho set "
+        clausulaSql += "nometipovinho = '" + tiraAspas(self.nomeTipoVinho) + "'  "
+        clausulaSql += "where idtipovinho = " + str(argId) + ";"
+
+        try:
+            self.cursor.execute(clausulaSql)
+            self.con.commit()
+        except:
+            dlg = wx.MessageDialog(None, clausulaSql, 'Erro ao atualizar os dados do tipo de vinho!', wx.OK | wx.ICON_ERROR)
+            result = dlg.ShowModal()
+
+    def delete(self, argId):
+        clausulaSql = 'delete from tipovinho '
+        clausulaSql += 'where idtipovinho = ' + str(argId) + ';'
+
+        try:
+            self.cursor.execute(clausulaSql)
+            self.con.commit()
+        except:
+            dlg = wx.MessageDialog(None, clausulaSql, 'Erro ao eliminar os dados do tipo de vinho!', wx.OK | wx.ICON_ERROR)
+            result = dlg.ShowModal()
+
+    def sqlBuscaTamanho(self, coluna):
+        if self.banco == 'postgres':
+            clausulaSql = "select character_maximum_length from INFORMATION_SCHEMA.COLUMNS "
+            clausulaSql += "where table_catalog = 'adega' and table_name = 'tipovinho'"
+            clausulaSql += "and column_name = '" + coluna + "';"
+        else:
+            clausulaSql = "select character_maximum_length from INFORMATION_SCHEMA.COLUMNS "
+            clausulaSql += "where table_name = 'tipovinho' and column_name = '" + coluna + "';"
+
+        try:
+            self.cursor.execute(clausulaSql)
+            row = self.cursor.fetchone()
+            while row != None:
+                return row[0]
+        except:
+            return 0
+
+
+
